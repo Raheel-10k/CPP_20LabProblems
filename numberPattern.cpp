b4b3bca980ee4490849ddec443590c5ab3827421
Raheel Kotwal <2023.raheelk@isu.ac.in>
2023-12-24 20:08:10 +0530

    minor edit

--- numberPattern.cpp
+++ numberPattern.cpp
@@ -4,31 +4,24 @@ using namespace std;
 
 int main() {
     int n;
-
     cout << "Enter the number of rows: ";
     cin >> n;
-
     for (int i = 1; i <= n; ++i) {
         for (int j = i; j < i * 2; ++j) {
             cout << j;
         }
-
         for (int j = i * 2 - 2; j >= i; --j) {
             cout << j;
         }
-
         cout << endl;
     }
-
     for (int i = n - 1; i >= 1; --i) {
         for (int j = i; j < i * 2; ++j) {
             cout << j;
         }
-
         for (int j = i * 2 - 2; j >= i; --j) {
             cout << j;
         }
-
         cout << endl;
     }
 
